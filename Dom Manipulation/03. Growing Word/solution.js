2ef6397845343bdd416e1f4e5a9fdfeacdf73d07
Dobromir Petrov <luzterin@gmail.com>
2020-10-03 15:52:47 +0300

    a

--- Dom Manipulation/03. Growing Word/solution.js
+++ Dom Manipulation/03. Growing Word/solution.js
@@ -1,4 +1,25 @@
 function growingWord() {
 
   //TODO...
+  let px =2;
+  let bottomEL = document.getElementById('exercise').lastElementChild;
+  let colorChanges = {
+    "blue": "green",
+    "green": "red",
+    "red": "blue"
+  };
+
+  if (!bottomEL.style.fontSize) {
+    bottomEL.setAttribute("style", `color: blue; ; font-size: ${px}px; text-Align: center`)
+
+  }
+  else {
+    let size = parseInt(bottomEL.style.fontSize);
+    let currentColor = bottomEL.style.color;
+    let currentPx = bottomEL.style["font-size"];
+    px = currentPx.substr(0, currentPx.length - 2) * 2;
+
+    bottomEL.setAttribute("style", `color:${colorChanges[currentColor]}; ; font-size: ${px}px; text-Align: center`);
+
+  }
 }
