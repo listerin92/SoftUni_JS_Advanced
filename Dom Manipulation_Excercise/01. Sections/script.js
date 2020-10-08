e38eac2193b02b8cb087bd0b39b9fb7f110ffa3d
Dobromir Petrov <luzterin@gmail.com>
2020-10-08 17:43:31 +0300

    aa

--- Dom Manipulation_Excercise/01. Sections/script.js
+++ Dom Manipulation_Excercise/01. Sections/script.js
@@ -1,3 +1,25 @@
 function create(words) {
-   console.log('TODO:...');
-}
+   let parent = document.getElementById('content');
+   
+   words.forEach(element => {
+      console.log(element);
+      let divEL = document.createElement('div');
+      divEL.id = element;
+      divEL.addEventListener('click', showInParagraph);
+      let pEL = document.createElement('p');
+      pEL.innerHTML = element;
+      pEL.style.display = 'none';
+
+      
+      
+      divEL.appendChild(pEL);
+      parent.appendChild(divEL);
+
+   });
+
+   function showInParagraph(event){
+      
+      event.path[0].children[0].style.display = 'block';
+
+   }
+}  
