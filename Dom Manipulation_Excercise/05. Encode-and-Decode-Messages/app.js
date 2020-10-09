610b71536515495ab0e18ba0fbd099cc8debcc5b
Dobromir Petrov <luzterin@gmail.com>
2020-10-09 18:20:40 +0300

    a

--- Dom Manipulation_Excercise/05. Encode-and-Decode-Messages/app.js
+++ Dom Manipulation_Excercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,42 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let encodeButton = document.querySelectorAll('button')[0];
+    let decodeButton = document.querySelectorAll('button')[1];
+    let messageToEncode = document.querySelectorAll('textarea')[0];
+    let messageToDecode = document.querySelectorAll('textarea')[1];
+        
+    encodeButton.addEventListener('click', encodeClick);
+    decodeButton.addEventListener('click', decodeClick);
+    let decodeClicked = false;
+
+
+    function encodeClick() {
+        messageToDecode.value = encode(messageToEncode.value);
+        messageToEncode.value = '';
+    }
+
+    function decodeClick() {
+        if (decodeClicked) {
+            messageToDecode.value = '';
+        }
+
+        messageToDecode.value = decode(messageToDecode.value);;
+        decodeClicked = true;
+
+    }
+
+    function encode(message) {
+        let encoded = '';
+        for (const character of message) {
+            encoded += String.fromCharCode(character.charCodeAt(0) + 1);
+        }
+        return encoded;
+    }
+
+    function decode(message) {
+        let decoded = '';
+        for (const character of message) {
+            decoded += String.fromCharCode(character.charCodeAt(0) - 1);
+        }
+        return decoded;
+    }
 }
--- Dom Manipulation_Excercise/06. Table-Search-Engine/solution.js
+++ Dom Manipulation_Excercise/06. Table-Search-Engine/solution.js
@@ -1,3 +1,34 @@
 function solve() {
-   //TODO...
+
+   let rows = Array.from(document.querySelectorAll('tbody > tr'));
+   let button = document.getElementById('searchBtn');
+   let inputValue = document.getElementById('searchField');;
+
+   button.addEventListener('click', find);
+
+
+   function find() {
+      let regex = new RegExp(inputValue.value, 'gim');
+      rows.map(e => {
+         e.classList.remove('select');
+         if (e.innerHTML.match(regex) !== null) {
+            e.className = 'select';
+         }
+      });
+      // for (const iterator of rows) {
+      //    iterator.removeAttribute('class');
+      // }
+
+      // for (const iterator of rows) {
+      //    for (const i of iterator.children) {
+      //       if (i.innerHTML === inputValue.value || i.innerHTML.includes(inputValue.value)) {
+      //          i.parentElement.className = 'select'
+
+      //       }
+      //    }
+      // }
+
+      inputValue.value = '';
+   }
+
 }
