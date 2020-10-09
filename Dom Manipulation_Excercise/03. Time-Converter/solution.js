2c8985c58db0908650b16a735172f5e7633f9719
Dobromir Petrov <luzterin@gmail.com>
2020-10-09 16:08:30 +0300

    a

--- Dom Manipulation_Excercise/03. Time-Converter/solution.js
+++ Dom Manipulation_Excercise/03. Time-Converter/solution.js
@@ -1,4 +1,43 @@
 function attachEventsListeners() {
+    //One day is equal to 24 hours/1440 minutes/86400 seconds
 
-    console.log('TODO:...');
+    let days = document.getElementById('days');
+    let hours = document.getElementById('hours');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+
+    let daysBtn = document.getElementById('daysBtn');
+    daysBtn.addEventListener('click', () => {
+
+        hours.value = Number(days.value) * 24;
+        minutes.value = days.value * 1440;
+        seconds.value = days.value * 86400;
+    });
+
+    let hoursBtn = document.getElementById('hoursBtn');
+    hoursBtn.addEventListener('click', () => {
+
+        days.value = hours.value / 24;
+        minutes.value = days.value * 1440;
+        seconds.value = days.value * 86400;
+    });
+
+    let minutesBtn = document.getElementById('minutesBtn');
+    minutesBtn.addEventListener('click', () => {
+
+        days.value = minutes.value / 1440;
+        hours.value = days.value * 24;
+        seconds.value = days.value * 86400;
+    });
+
+    let secondsBtn = document.getElementById('secondsBtn');
+    secondsBtn.addEventListener('click', () => {
+
+        days.value = seconds.value / 86400;
+        hours.value = days.value * 24;
+        minutes.value = days.value * 1440;
+
+    });
+
+
 }
--- Dom Manipulation_Excercise/04. Locked-Profile/app.js
+++ Dom Manipulation_Excercise/04. Locked-Profile/app.js
@@ -1,3 +1,19 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const btns = [...document.getElementsByTagName('button')];
+    btns.forEach(btn => btn.addEventListener('click', clickFunc));
+
+    function clickFunc(event) {
+
+        let unlockStatus = event.target.parentElement.getElementsByTagName('input')[1].checked;
+        let shown = event.target.parentElement.getElementsByTagName('div')[0].style.display;
+        if (unlockStatus || shown == 'none') {
+            event.target.parentElement.getElementsByTagName('div')[0].style.display = 'block';
+            event.target.textContent = 'Hide it';
+
+        }
+        else if (unlockStatus && shown) {
+            event.target.parentElement.getElementsByTagName('div')[0].style.display = 'none';
+            event.target.textContent = 'Show more';
+        }
+    };
 }
